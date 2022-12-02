2857953529084c194770b5ee378df3252b155e52
Natalia-Bogatyreva <rogova_nu@mail.ru>
2022-12-02 13:31:03 +0500

    parity of numbers

---
+++ Task6/Program.cs
@@ -0,0 +1,14 @@
+﻿// Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+int numberA = 0;
+
+Console.Write("Введите число:");
+numberA = Convert.ToInt32(Console.ReadLine());
+
+if (numberA % 2 == 0 )
+{
+    Console.WriteLine(numberA +" - четное число");
+}
+else
+{
+   Console.WriteLine(numberA +" - нечетное число");
+}
